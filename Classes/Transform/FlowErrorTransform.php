0da65788967cfb36ae2b783ebe7557264b8df935
Torsten Blindert <info@by-torsten.com>
2018-12-11 15:28:49 +0100

    FEATURE: Use special flow error transform to log errors and display in result

---
+++ Classes/Transform/FlowErrorTransform.php
@@ -0,0 +1,54 @@
+<?php
+namespace t3n\GraphQL\Transform;
+
+use GraphQL\Error\Error;
+use Neos\Flow\Annotations as Flow;
+use GraphQL\Executor\ExecutionResult;
+use GraphQLTools\Transforms\Transform;
+use Neos\Flow\Log\ThrowableStorageInterface;
+
+class FlowErrorTransform implements Transform
+{
+    /**
+     * @Flow\Inject
+     * @var ThrowableStorageInterface
+     */
+    protected $throwableStorage;
+
+    /**
+     * @Flow\InjectConfiguration("includeExceptionMessageInOutput")
+     * @var bool
+     */
+    protected $includeExceptionMessageInOutput;
+
+    /**
+     * @param ExecutionResult $result
+     * @return ExecutionResult
+     */
+    public function transformResult(ExecutionResult $result): ExecutionResult
+    {
+        $result->errors = array_map(function (Error $error) {
+            $previousError = $error->getPrevious();
+            if (! $previousError instanceof Error) {
+                $message = $this->throwableStorage->logThrowable($previousError);
+
+                if (! $this->includeExceptionMessageInOutput) {
+                    $message = preg_replace('/.* - See also: (.+)\.txt$/', 'Internal error ($1)', $message);
+                }
+
+                return new Error(
+                    $message,
+                    $error->getNodes(),
+                    $error->getSource(),
+                    $error->getPositions(),
+                    $error->getPath(),
+                    $previousError
+                );
+            }
+
+            return $error;
+        }, $result->errors);
+
+        return $result;
+    }
+}
